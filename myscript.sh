1c59186ace0676c17e941dc11efcf3228678f61c
Lawrence <macknever@gmail.com>
2021-12-30 09:14:22 -0800

    case statement

--- myscript.sh
+++ myscript.sh
@@ -48,13 +48,50 @@
 #     echo "$NUM1 is less than $NUM2"
 # fi
 
+########
+# val1 -eq val2 Returns true if the values are equal
+# val1 -ne val2 Returns true if the values are not equal
+# val1 -gt val2 Returns true if val1 is greater than val2
+# val1 -ge val2 Returns true if val1 is greater than or equal to val2
+# val1 -lt val2 Returns true if val1 is less than val2
+# val1 -le val2 Returns true if val1 is less than or equal to val2
+########
+
 
 # FILE CONDITIONS
 
-FILE="test.txt"
-if [ -e "$FILE" ]
-then
-    echo "$FILE exists"
-else 
-    echo "$FILE does NOT exist"
-fi
+# FILE="test.txt"
+# if [ -e "$FILE" ]
+# then
+#     echo "$FILE exists"
+# else 
+#     echo "$FILE does NOT exist"
+# fi
+
+########
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is an executable
+########
+
+# CASE STATEMENT
+
+read -p "Are you 21 or over? Y/N " ANSWER
+case "$ANSWER" in
+    [yY] | [yY][eE][sS]) # ) to end the condition
+    echo "You can have a beer :)"
+    ;; # ;; to end this case
+    [nN] | [nN][oO])
+    echo "Sorry!"
+    ;;
+    *) # default, none of above happen
+    echo "PLease enter y/yes or n/no"
+    ;;
+esac
+
