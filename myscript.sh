217dfe8366fb655f78a532eb2594cfb94d08ad15
Lawrence <macknever@gmail.com>
2021-12-29 17:03:53 -0800

    ECHO, VARIABLES, USER INPUT, IF STATEMENT

---
+++ myscript.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# ECHO COMMAND
+# echo Hello World!
+
+# VARIABLES
+# Uppercase by convention
+
+NAME="Emily"
+
+
+# USER INPUT
+
+# read -p "Enter your name: " NAME
+# echo Hello $NAME
+
+# SIMPLE IF STATEMENT
+
+# if [ $NAME==Lawrence ]
+# then
+#     echo Lawrence
+# fi
+
+
+
+# IF-ELSE
+
+if [ "$NAME" == "Lawrence" ]
+then
+    echo "a"
+elif [ "$NAME" == "Emily" ]
+then
+    echo "b"
+else 
+    echo "NOT a"
+fi
